26b41c2f62cb991cf7e693e52b7060b4ab9e9481
CyberCeli <Aracelihair@gmail.com>
2021-08-02 15:31:46 -0600

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-
-//import logo from './logo.svg';
-
-import Navbar from "./Navbar"
-import "./Navbar.css"
-import {Route} from "react-router-dom"
-import Background from  "./Background"
-import MainNav from "./MainNav"
-=======
 import Navbar from "./components/nav/NavBar"
 import "./components/nav/NavBar.css"
 import {Route} from "react-router-dom"
 import Background from  "./components/background/Background"
 
->>>>>>> stephen-dev
 
 import './App.css';
 import Home from './screens/home/Home';
@@ -26,54 +15,16 @@ import MainNavBar from "./components/nav/MainNavbar"
 
 
 
+
 function App() {
   return (
     <div className="App">
 
       <Navbar/>
-<<<<<<< HEAD
-      <MainNav/>
-       <Background/>
-      <Route exact path = "/">
-        {/* <Home/> */}
-      </Route>
-      <Route path="/Signup">
-
-      </Route>
-      <Route path="/Signin">
-
-      </Route>
-      <h1>Trending Now</h1>
-<div className = "together">
-            <div className = "first">
-            <div className = "move">
-            <h3>
-Elon Musk's SpaceX launches 143 satellites on single rocket, sets world record.</h3>
-            </div>
-            
-            </div>
-            <div className = "second">
-           <h3>Happy Spider-Man day</h3>
-          </div>
-          <div className = "third">
-            <h3>Activision hit with another lawsuit as female employees are in a frenzy.</h3>
-          </div>
-          <div className = "fourth">
-            <h3>EA play show Dead Space. Could this be EA's big comeback</h3>
-          </div>
-          </div>
-
-      {/* <Route to="/">
-        <Home />
-      </Route>
-      <Route to="/create-post">
-        <CreatePost />
-=======
       <MainNavBar/>
       <Background/>
       <Route exact path = "/">
         <Home/>
->>>>>>> stephen-dev
       </Route>
       {/* <Route to="/sign-up">
         <SignUp />
@@ -83,16 +34,10 @@ Elon Musk's SpaceX launches 143 satellites on single rocket, sets world record.<
       </Route>
       <Route to="/edit-post">
         <Edit />
-<<<<<<< HEAD
-      </Route> */}
-
-
-=======
       </Route>
       <Route to="/create-post">
         <CreatePost />
       </Route> */}
->>>>>>> stephen-dev
     </div>
   );
 }
