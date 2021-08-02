9c10b843c5b8b0802e2a02babab86ff3918ebe6c
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-02 15:41:37 -0400

    controllers update

--- routes/thread.js
+++ routes/thread.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
+import { createThread, getThreads, getThread, updateThread, deleteThread } from "../controllers/threads.js"
 
 const router = Router()
 
 router.post("/create-thread", restrict, createThread)
 router.get("/threads", getThreads)
+router.get("/threads", getThread)
 router.put("/threads/:id", restrict, updateThread)
 router.delete("/threads/:id", restrict, deleteThread)
 
