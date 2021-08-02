087e2a7c2365f3f26536087195a43d62392ed80b
stephenharrity <sjh401@gmail.com>
2021-08-02 17:35:13 -0500

    finally

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import CreatePost from './screens/create/Create';
 import SignIn from './screens/sign-in/SignIn';
 import SignUp from './screens/sign-up/SignUp';
 import Edit from './screens/edit/Edit';
-import MainNavBar from "./components/nav/MainNavbar"
+import MainNavBar from "./components/nav/MainNavBar"
 
 
 
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Footer from './Footer'
-import MainNavBar from './nav/MainNavbar'
+import MainNavBar from './nav/MainNavBar'
 
 
 export default function Layout(props) {
