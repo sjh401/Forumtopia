be4411e7f70ef14130a97d2b507fa94a2a47fa9b
Ashley Sands <ashley.sands01@gmail.com>
2021-08-02 13:50:50 -0500

    added post controllers except for delete function

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
+import db from "./db/index.js"
+import routes from "./routes/index.js"
 
 const app = express()
 const PORT = process.env.PORT || 4567
@@ -9,6 +11,11 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 
-app.use("/api",)
+app.use("/api", routes)
 
+app.get("/", (req, res) => res.send("API DOCUMENTATION"))
 
+debug.once("connectedd", () => {
+  console.log("Connected to MongoDB")
+  app.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
+})
