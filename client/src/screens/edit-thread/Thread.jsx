c316a7fc7e1145023ab7e34cf38953c05d863200
stephenharrity <sjh401@gmail.com>
2021-08-04 15:47:40 -0500

    solo thread screen created and functional

--- client/src/screens/edit-thread/Thread.jsx
+++ client/src/screens/edit-thread/Thread.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import Layout from '../../components/Layout/Layout';
+import { getThread, updateThread } from '../../services/thread';
 
-export default function Thread() {
+export default function Thread(props) {
+    const [ thread, setThread ] = useState([]);
+    const { id } = useParams();
+    
+    useEffect(() => {
+        const fetchThread = async () => {
+            const thread = await getThread(id);
+            console.log(thread)
+            setThread(thread);
+        }
+        fetchThread();
+    }, [id]);
+
+    const displayEditLik = () => {
+        if(thread.userId === props.user?.id) {
+            return <Link to={`/thread-edit/${thread._id}`} style={{color:"blue"}}>Edit</Link>
+        }
+    }
+    console.log(props.user)
     return (
-        <div>
-            here's where the signle thread will be and updated thread will redirect to
-        </div>
+        <Layout>
+            <div key={thread.userId} >
+                <h4>{thread.title}</h4>
+                <img style={{width: "100px", height: "100px"}} src={thread.imgUrl}/>
+                <p>{thread.body}</p>
+                {displayEditLik()}
+            </div>
+        </Layout>
     )
 }
