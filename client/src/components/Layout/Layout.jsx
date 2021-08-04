691570d2675c5ac1dce0b888d714631e262d7cec
Timothy <timlundy74@gmail.com>
2021-08-04 12:28:06 -0400

    update

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import Nav from '../Nav/MainNavBar'
+import Nav from "../Nav/MainNavBar"
 import Footer from "../Footer/Footer"
 
 const Layout = (props) => (
---
+++ client/src/components/Nav/MainNavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Mobile.css"
+
+export default function MainNavBar(props) {
+  return (
+        <div className = "menu">
+              <div className = "name">
+              <Link to="/">Forumtopia</Link>
+              </div>
+              <div className="space">
+                <Link to="/sign-in">Sign In</Link>
+                <Link to="/sign-up">Sign Up</Link>
+              </div>
+          </div>
+        )
+}
