d83ccaa7f8298df8cad428de58641ada97520cbe
Seth Lee <sethlee@seths-air.attlocal.net>
2021-08-05 15:26:04 -0400

    navbar signout edits

--- client/src/components/Nav/MainNavBar.jsx
+++ client/src/components/Nav/MainNavBar.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+import { NavLink } from 'react-router-dom'
 import "./Navs.css"
 
-export default function MainNavBar(props) {
+const authenticatedOptions = (
+  <>
+    <NavLink to="/signout">Sign Out</NavLink>
+    </>
+)
+const unauthenticatedOptions = (
+  <>
+    <NavLink to="/sign-in">Sign In</NavLink>
+    <NavLink to="/sign-up">Sign Up</NavLink>
+    {/* <NavLink to="/signout">Sign Out</NavLink> */}
+    </>
+)
+
+// maybe add posts and threads to here? not sure.
+const alwaysOptions = (
+  <>
+    <NavLink to=""></NavLink>
+    <NavLink to=""></NavLink>
+    </>
+)
+
+const MainNavBar = ({ user }) => {
   return (
-        <div className = "menu">
-              <div className = "name">
-              <Link to="/">Forumtopia</Link>
-              </div>
-              <div className="space">
-                <Link to="/sign-in">Sign In</Link>
-        <Link to="/sign-up">Sign Up</Link>
-        <Link to="signout">Sign Out</Link>
-              </div>
-          </div>
-        )
+    <nav>
+      <div className="menu">
+        <NavLink to="/">Forumtopia</NavLink>
+        {/* he had classname for the divs underneath, I didn't but put in if you want */}
+        <div>
+          {user && <div>Welcome, {user.username}</div>}
+          {alwaysOptions}
+          {user ? authenticatedOptions : unauthenticatedOptions}
+        </div>
+      </div>
+    </nav>
+      
+  )
 }
+
+// export default function MainNavBar(props) {
+//   return (
+//         <div className = "menu">
+//               <div className = "name">
+//               <Link to="/">Forumtopia</Link>
+//               </div>
+//               <div className="space">
+//                 <Link to="/sign-in">Sign In</Link>
+//         <Link to="/sign-up">Sign Up</Link>
+//         <Link to="signout">Sign Out</Link>
+//               </div>
+//           </div>
+//         )
+// }
+
+export default MainNavBar
