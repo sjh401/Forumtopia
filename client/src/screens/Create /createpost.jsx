a77cf9aa2d363fdb38ee3542a619d15c1f589d78
CyberCeli <Aracelihair@gmail.com>
2021-08-05 14:41:35 -0600

    update

--- client/src/screens/Create /createpost.jsx
+++ client/src/screens/Create /createpost.jsx
@@ -1,42 +1,65 @@
 import { useState } from 'react';
-import { useHistory } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import Layout from '../../components/Layout/Layout';
+import { createPost } from '../../services/post';
+import { useParams } from 'react-router';
 
 
 
 
-export default function CreatePost(props) {
+export default function PostCreate(props)  {
+  const [input, setInput] = useState({
+    body: "",
+    imgUrl: ""
+  })
+  const { id } = useParams();
   
-  const [input, setInput] = useState({ body: "", imgUrl: "" });
-  const history = useHistory();
   
+  const [isCreated, setCreated] = useState(false)
+
   const handleChange = (e) => {
-    const { id, value } = e.target
-    
+    const { name, value } = e.target
     setInput(prevInput => ({
       ...prevInput,
-      [id]: value
+      [name]: value,
     }))
   }
-
   const handleSubmit= async (e) =>{
     e.preventDefault();
-    await CreatePost(input);
-    history.pushState("/");
+    const created = await createPost(id, input);
+    setCreated ({ created })
   };
+
+  if (isCreated) {
+    return <Redirect to={`/post`} />
+  }
 
   return (
     <Layout>
       Create a Post 
-      <form onsubmit={handleSubmit} className="form-horizontal">
-        <label>Body:</label>
-        <br />
-        <input id="body" value={input.body} onChnage={handleChange} />
+      <form className="create-form" onSubmit={handleSubmit}>
+       <br />
+        <textarea
+          className='textarea-body'
+          rows={10}
+          placeholder='body'
+          value={input.body}
+          name='body'
+          required
+          onChange={handleChange}
+          />
         <br />
         <label>Image:</label>
-        <input id="imgUrl" value={input.imgUrl} onChange={handleChange} />
+        <input 
+          className="input-image-link"
+          placeholder='Image Link'
+          value={input.imgUrl}
+          name='imgUrl'
+          required
+          onChange={handleChange}
+        />
         <br />
-        <button>Submit</button>
+        <button type ='submit' className='submit-button'>Submit</button>
 
       </form>
         
