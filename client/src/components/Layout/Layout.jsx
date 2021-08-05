2797c3e14bd38f5801e9695e49480a2e584d10b2
Timothy <timlundy74@gmail.com>
2021-08-05 13:44:45 -0400

    update

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import MobileNav from "../MobileNav/MobileNav"
 import Footer from "../Footer/Footer"
 import MainNavBar from "../Nav/MainNavBar"
+import "./Layout.css"
 
 const Layout = (props) => (
   <div>
-    <MainNavBar user={props.user} />
+    <MainNavBar className="main" user={props.user} />
     <MobileNav/>
     <div>{props.children}</div>
     <Footer />
