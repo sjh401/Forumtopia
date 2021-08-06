6d83e2b0a54015d4876cba3bd179da0914ecae90
Ashley Sands <ashley.sands01@gmail.com>
2021-08-06 09:41:54 -0500

    css changes

--- client/src/components/Nav/MainNavBar.jsx
+++ client/src/components/Nav/MainNavBar.jsx
@@ -5,8 +5,8 @@ import "./Navs.css"
 const authenticatedOptions = (
   <>
     <NavLink to="/signout">Sign Out</NavLink>
-    
-    </>
+
+  </>
 )
 const unauthenticatedOptions = (
   <>
@@ -14,7 +14,7 @@ const unauthenticatedOptions = (
     <NavLink to="/sign-up">Sign Up</NavLink>
     <NavLink to="/threads"></NavLink>
     {/* <NavLink to="/signout">Sign Out</NavLink> */}
-    </>
+  </>
 )
 
 // maybe add posts and threads to here? not sure.
@@ -22,23 +22,23 @@ const alwaysOptions = (
   <>
     <NavLink to=""></NavLink>
     <NavLink to=""></NavLink>
-    </>
+  </>
 )
 
 const MainNavBar = (props) => {
   return (
     <nav>
-      <div className="menu">
-        <NavLink to="/">Forumtopia</NavLink>
+      <div className="nav-bar">
+        <NavLink className="site-logo" to="/">Forumtopia</NavLink>
         {/* he had classname for the divs underneath, I didn't but put in if you want */}
-        <div>
+        <div className="nav-links">
           {props.user && <div>Welcome, {props.user.username}</div>}
           {alwaysOptions}
           {props.user ? authenticatedOptions : unauthenticatedOptions}
         </div>
       </div>
     </nav>
-      
+
   )
 }
 
