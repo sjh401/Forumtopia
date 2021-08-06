74a76e4d5add6452bb32dd66ee7e0d41bb7b344e
Ashley Sands <ashley.sands01@gmail.com>
2021-08-06 10:43:52 -0500

    changed some css

--- client/src/components/Nav/MainNavBar.jsx
+++ client/src/components/Nav/MainNavBar.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import "./Nav.css"
+
+const authenticatedOptions = (
+  <>
+    <NavLink to="/signout">Sign Out</NavLink>
+    <NavLink to="/threads-create">Create a Thread</NavLink>
+
+  </>
+)
+const unauthenticatedOptions = (
+  <>
+    <NavLink to="/sign-in">Sign In</NavLink>
+    <NavLink to="/sign-up">Sign Up</NavLink>
+    <NavLink to="/threads"></NavLink>
+    {/* <NavLink to="/signout">Sign Out</NavLink> */}
+  </>
+)
+
+// maybe add posts and threads to here? not sure.
+const alwaysOptions = (
+  <>
+    <NavLink to=""></NavLink>
+    <NavLink to=""></NavLink>
+  </>
+)
 
 export default function MainNavBar(props) {
   return (
-    <div className="main-navbar-container">
-      <div className="name">
-        <Link className="navbar-links" to="/">Forumtopia</Link>
+    <nav>
+      <div className="nav-bar">
+        <NavLink className="site-logo" to="/">Forumtopia</NavLink>
+        <div className="nav-links">
+          {props.user && <div>Welcome, {props.user.username}</div>}
+          {alwaysOptions}
+          {props.user ? authenticatedOptions : unauthenticatedOptions}
+        </div>
       </div>
-      <div className="user-login-signup">
-        <Link className="navbar-links" to="/sign-in">Sign In</Link>
-        <Link className="navbar-links" to="/sign-up">Sign Up</Link>
-      </div>
-    </div>
+    </nav>
+
   )
 }
+
--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -8,52 +8,109 @@ import { verify } from "../../services/user"
 
 
 
-export default function HomeScreen(props) {
-  const [threads, setThreads] = useState([])
-  const [user, setUser] = useState(null)
+// export default function HomeScreen(props) {
+//   const [threads, setThreads] = useState([])
+//   const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    const verifyUser = async () => {
-      setUser(await verify())
-    }
-    verifyUser()
-  }, [])
+//   useEffect(() => {
+//     const verifyUser = async () => {
+//       setUser(await verify())
+//     }
+//     verifyUser()
+//   }, [])
 
-  useEffect(() => {
-    const fetchThreads = async () => {
-      let data = await getThreads();
-      setThreads(data)
-    }
-    fetchThreads()
-  }, [])
+//   useEffect(() => {
+//     const fetchThreads = async () => {
+//       let data = await getThreads();
+//       setThreads(data)
+//     }
+//     fetchThreads()
+//   }, [])
 
+
+//   return (
+//     <Layout>
+//       <h1 className="trend-text">Trending Now</h1>
+//       <div className="trending-now-container">
+//         <div className="first-image">
+//           <h4>Elon Musk's SpaceX launches 143 satellites on single rocket, sets world record.</h4>
+//         </div>
+//         <div className="second-image">
+//           <h4>Scarlett Johansson sues Disney for breach of contract.</h4>
+//         </div>
+//         <div className="third-image">
+//           <h4>Activision hit with another lawsuit as female employees are in a frenzy.</h4>
+//         </div>
+//         <div className="fourth-image">
+//           <h4>EA play show Dead Space. Could this be EA's big comeback?</h4>
+//         </div>
+//       </div>
+//       <div className="threads-home">
+//         {threads.map((thread) => {
+//           return <ThreadCard key={thread._id} thread={thread} user={user} />
+//         })}
+//       </div>
+//     </Layout>
+
+//   )
+// }
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import homeData from "./homeData"
+
+console.log(homeData)
+
+const useStyles = makeStyles({
+  root: {
+    width: 345,
+  },
+});
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
 
   return (
     <Layout>
-      <h1 className="trend">Trending Now</h1>
-      <div className="together">
-        <div className="first">
-          <div className="move">
-            <h3>They Elon Musk's SpaceX launches 143 satellites on single rocket, sets world record.</h3>
-          </div>
-        </div>
-        <div className="second">
-          <h3>scarlett johansson sues Disney for breach of contract</h3>
-        </div>
-        <div className="third">
-          <h3>Activision hit with another lawsuit as female employees are in a frenzy.</h3>
-        </div>
-        <div className="fourth">
-          <h3>EA play show Dead Space. Could this be EA's big comeback</h3>
-        </div>
-      </div>
-      <div className="threads-home">
-        {threads.map((thread) => {
-          return <ThreadCard key={thread._id} thread={thread} user={user} />
+      <div className="main-card-container">
+        {homeData.map((data) => {
+          return (
+            <div className="trend-card-container">
+              <Card className={classes.root}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={data.imageUrl}
+                  />
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {data.text}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          )
         })}
+
       </div>
     </Layout>
-
-  )
+  );
 }
 
