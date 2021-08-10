be56d94225132aa47eb09dcfd420561ac68b7b1f
stephenharrity <sjh401@gmail.com>
2021-08-10 09:06:27 -0500

    last issue

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import { getCategories } from '../../services/category'
 import { Link } from 'react-router-dom';
 
